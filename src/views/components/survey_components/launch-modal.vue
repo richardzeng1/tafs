13726b45cfa2e4939834d09a96acdc27033cc5a4
Run Zhao Zeng <run.zeng@mail.utoronto.ca>
2018-07-27 17:21:57 -0400

    Implement wrapper

---
+++ src/views/components/survey_components/launch-modal.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <v-btn @click="dialog = true">Launch</v-btn>
+        <v-dialog v-model="dialog" width="500">
+            <v-card>
+                <v-card-title>Launch Survey</v-card-title>
+                <v-card-text>Launch</v-card-text>
+
+                <v-card-actions>
+                  <v-spacer></v-spacer>
+                  <v-btn
+                    color="primary"
+                    flat
+                    @click="dialog = false"
+                  >
+                    I accept
+                  </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+          dialog:false
+      }
+  }
+  }
+</script>
--- src/views/components/survey_wrapper.vue
+++ src/views/components/survey_wrapper.vue
@@ -1,24 +1,32 @@
+
 <template>
 <div>
       <SurveySummary :summary_package="summary_package" v-on:show="change_display"> </SurveySummary>
       <div v-if="show">
-          <button v-if="!is_instance" type="button" name="button">Launch</button>
+          <button v-if="!is_instance" type="button" name="button" v-on:click="display_modal=true">Launch</button>
+          <button type="button" name="button" v-on:click="display_modal=true">Launch</button>
           <button type="button" name="button">Clone</button>
           <button v-if="!is_instance" type="button" name="button">Edit</button>
           <button v-if="is_instance" type="button" name="button">Allow Instructor Access</button>
+          <v-app>
+                <v-content>
+                    <LaunchModal></LaunchModal>
+            </v-content>
+      </v-app>
       </div>
 </div>
 </template>
 
 <script>
 import SurveySummary from "./response_components/survey_summary.vue";
-
+import LaunchModal from "./survey_components/launch-modal.vue";
 export default {
     name: "SurveyWrapper",
     props: ["summary_package", "is_instance"],
     data: function() {
         return {
-            show: false
+            show: false,
+            display_modal: false
         };
     },
     methods: {
@@ -27,7 +35,8 @@ export default {
         }
     },
     components: {
-        SurveySummary
+        SurveySummary,
+        LaunchModal
     }
 };
 </script>
